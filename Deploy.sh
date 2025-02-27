2553b375a8b8c3a4ee330327a05f53b58af68c3d
Kevin Medina <kevinmedina2509Jr@gmail.com>
2025-02-26 18:38:47 -0600

    NROK Script

---
+++ Deploy.sh
@@ -0,0 +1,21 @@
+REPO_DIR="/var/www/html/Resume"
+
+# Apagar NGINX y NGROK
+sudo systemctl stop nginx
+pkill -f ngrok
+
+echo "NGINX y NGROK detenidos."
+
+# Actualizar el repositorio
+echo "Actualizando el repositorio..."
+cd "$REPO_DIR" || exit
+git pull
+
+echo "Repositorio actualizado..."
+
+# Encender NGINX
+sudo systemctl start nginx
+echo "NGINX iniciado"
+
+#Encender NROK
+ngrok http 80
